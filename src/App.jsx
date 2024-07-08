a9b9859413f05fc0935abb0fa542a3f28390bec8
Kayla Hunt <huntkay1@msu.edu>
2024-07-08 19:59:36 -0400

    Add Experience form

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,14 @@
 import PersonalInfo from './components/PersonalInfo.jsx';
 import Education from './components/EducationInfo.jsx';
-import { useState } from 'react';
 import Experience from './components/ExperienceInfo.jsx'
 
 function App() {
-
-  // const [formData, setFormData] = useState([]);
-  // const [displayForm, setDisplayForm] = useState(true);
-  
-
-  // // Function to save form data to formData array
-  // function updateFormData(newEntry) {
-  //     setFormData([...formData, newEntry]);
-  // }
-
-  // //Function to update entry's data
-  // function updateEntry(newEntry) {
-  //     const updatedFormData = [...formData];
-  //     updatedFormData[editingIndex] = newEntry;
-  //     setFormData(updatedFormData);
-  // }
-
-  // function deleteEntry(index) {
-  //   const selectedEntry = formData[index];
-  //   setFormData(formData.filter(entry => 
-  //       entry.id !== selectedEntry.id
-  //   ));
-  // }
-
-
-
-
 
   return(
     <div>
       <PersonalInfo />
       <Education />
-    <Experience />
+      <Experience />
     </div>
   )
   
