0f8c7e80f8ab8d190d869f4193973a0aefecd9ff
Kayla Hunt <huntkay1@msu.edu>
2024-07-15 18:09:24 -0400

    Style resume header

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,14 +1,15 @@
-function Resume({ educationData, experienceData}) {
+function Resume({ formData }) {
     return(
         <div>
             <div className='resume-header'>
-
+                <h1 id='header-name'>{formData.personalInfo.name}</h1>
+                <div id='contact-info'>
+                    <h2>{formData.personalInfo.email}</h2>
+                    <h2>{formData.personalInfo.phone}</h2>
+                    <h2>{formData.personalInfo.location}</h2>
+                </div>
             </div>
-            {/* <ul>
-                {educationData.map(dataEntry => (
-                    <li>{dataEntry.school}</li>
-                ))}
-            </ul> */}
+            
         </div>
 
     )
