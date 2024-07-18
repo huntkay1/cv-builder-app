56caa166ff9f38b047b14e1e933e10d360a6cca8
Kayla Hunt <huntkay1@msu.edu>
2024-07-17 23:15:09 -0400

    Remove PersonalInfo expand toggle

--- src/components/PersonalInfo.jsx
+++ src/components/PersonalInfo.jsx
@@ -1,16 +1,7 @@
-import { useState } from 'react'
-import downIcon from '../assets/down.svg'
-import upIcon from '../assets/up.svg'
 import personalIcon from '../assets/personal.svg'
 
 
 function PersonalInfo({ handleFormDataUpdate, formData }) {
-    const [isExpanded, setIsExpanded] = useState(false);
-
-
-    function toggleExpand() {
-        setIsExpanded(!isExpanded);
-    };
 
     return(
         <div className='form-container' id='personalInfo'>
@@ -19,9 +10,8 @@ function PersonalInfo({ handleFormDataUpdate, formData }) {
                     <img src={personalIcon}></img>
                     <h2 className='form-title'>Personal Details</h2>
                 </div>
-                <img className='expand-toggle' onClick={toggleExpand} src={isExpanded ? downIcon : upIcon}/>
             </div>
-            <form className={isExpanded ? '' : 'collapsed'}>
+            <form >
                 <div className='input-container'>
                     <label htmlFor='name'>Full Name</label>
                     <input type='text' name='name' id='name' value={formData.name} onChange={handleFormDataUpdate}></input>
